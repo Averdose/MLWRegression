592ac5dbc5b7986bc76f5133a7860af1b4107daa
Averdose <mrslon@kth.se>
2019-04-12 10:54:41 +0200

    stable version before cleanup

--- MLWRegression/MLWRegression/utility.c
+++ MLWRegression/MLWRegression/utility.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#define LINELEN 99
 void computeResult(float** xes, float* beta, int size, int argNum, float* outy) {
 	for (int i = 0; i < size; i++)
 	{
@@ -26,11 +26,9 @@ void printFunction(float* beta, int size)
 
 void transpose(float** arr, float* ar, int s1, int collumn)
 {
-	//printf("column=%d\n", collumn);
 	for (int i = 0; i < s1; i++)
 	{
 		ar[i] = arr[i][collumn];
-		//printf("%f\n", arr[i][collumn]);
 	}
 }
 void print2(float* arr, int s1)
@@ -66,70 +64,92 @@ void printResult(float* beta, float* prediction, int argNum, int testSize, float
 	}
 }
 
-void readLine2(char* line2, char*** arguments2, int iterator)
+void removeNL(char* line)
 {
-	char* *next;
-	char* arg = strtok(line2, " ");
+	for (int i = 0; i < LINELEN; i++)
+	{
+		if (line[i] == '\n')
+		{
+			line[i] = 0;
+		}
+	}
+}
+
+void readLine(char* line, char*** arguments, int iterator)
+{
+	char* arg = strtok(line, " ");
 	int i = 0;
 	while (arg != NULL)
 	{
-		arguments2[iterator][i] = arg;
+		strcpy(arguments[iterator][i], arg);
+		removeNL(arguments[iterator][i]);
 		arg = strtok(NULL, " ");
 		i++;
 		
 	}
 	if (i == 5)
 	{
-		arguments2[iterator][5] = "10000";
+		arguments[iterator][5] = "10000";
 	}
 	//line2 = NULL;
 }
 
-void readConfig(char* path, char*** arguments, int* testNum)
+void readConfig(char* path, char*** arguments, int* testNum, int consoleArgs)
 {
-	FILE* f = openFile(path);
+	FILE *f;
+	char line[LINELEN];
+	int iterator = 0;
+	f = fopen(path, "r");
 	if (f == NULL)
 	{
 		return -1;
 	}
-	int iterator = 0;
-	char* line = (char*)malloc(sizeof(char) * 9999);
-	char c = 's';
-	while (c != NULL)
-	{
-		c = fgets(line, 9999, f);
-		readLine2(line, arguments, iterator);
+	while (fgets(line,9999,f)!=NULL) {
+		printf("%s", line);
+		readLine(line, arguments, iterator);
 		iterator++;
 	}
+	printf("\n");
 	fclose(f);
 	*testNum = iterator;
 }
 
-void writeToFile(char* path, float* yTrue, float* yPredict, int size, float mse, float sse, float mae) {
+int openFileW(FILE* f, char* path)
+{
+	
+	if (f == NULL)
+	{
+		return -1;
+	}
+	return 1;
+}
 
-	char* pathStats = malloc(sizeof(char) * 999);
-	char* pathCompare = malloc(sizeof(char) * 999);
+void writeToFile(char* path, float* yTrue, float* yPredict, int size, float mse, float sse, float mae) {
+	char* pathStats = malloc(sizeof(char) * LINELEN);
+	char* pathCompare = malloc(sizeof(char) * LINELEN);
 	strcpy(pathStats, path);
 	strcpy(pathCompare, path);
 	strcat(pathStats, "Statistics.txt");
 	strcat(pathCompare, "Comparision.txt");
-	FILE * fStats = fopen(pathStats, 'w');
-	if (fStats == NULL)
+	int k = 0;
+	FILE *f;
+	f = fopen(pathStats, "w");
+	if (f == NULL)
 	{
 		return -1;
 	}
-	FILE * fComp = fopen(pathCompare, 'w');
+	FILE *fComp = fopen(pathCompare, "w");
 	if (fComp == NULL)
 	{
 		return -1;
 	}
-	fprintf(fStats, "MSE=%f, SSE=%f, MAE=%f", mse, sse, mae);
-	fprintf(fComp, "YTRUE,YPREDICT");
+	fprintf(f, "MSE=%f, SSE=%f, MAE=%f", mse, sse, mae);
+	fprintf(fComp, "YTRUE,YPREDICT\n");
 	for (int i = 0; i < size; i++)
 	{
 		fprintf(fComp, "%f,%f\n", yTrue[i], yPredict[i]);
 	}
 	fclose(fComp);
-	fclose(fStats);
+	fclose(f);
 
 }
